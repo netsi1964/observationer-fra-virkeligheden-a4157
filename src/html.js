444030b13cc22b79c9360384e873af9723f2c9b4
Sten Hougaard <netsi1964@gmail.com>
2021-03-02 07:49:43 +0100

    Added google analytics

--- src/html.js
+++ src/html.js
@@ -6,6 +6,15 @@ export default function HTML(props) {
     <html {...props.htmlAttributes}>
       <head>
         {props.headComponents}
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-00EQBWCZ4L"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-00EQBWCZ4L');
+</script>
       </head>
       <body {...props.bodyAttributes}>
         {props.preBodyComponents}
